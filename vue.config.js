a9505e7e97a6368d574c10632e42761c531ee688
Lushenggang <lushg-tcxg@qq.com>
2020-03-08 21:53:55 +0800

    push

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? './' : '/'
+  publicPath: process.env.NODE_ENV === "production" ? './' : '/',
+  productionSourceMap: false
 }

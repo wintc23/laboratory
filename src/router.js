d6cad5dd64bd322252f1a04105c1128109b7d713
Lushenggang <lushg-tcxg@qq.com>
2020-03-08 22:06:12 +0800

    push

--- src/router.js
+++ src/router.js
@@ -2,31 +2,26 @@ import VueRouter from 'vue-router'
 import Vue from 'vue'
 Vue.use(VueRouter)
 
-import Home from '@/pages/Home'
-import Tree from '@/pages/Tree'
-import SearchHighlight from '@/pages/SearchHighlight'
-import Watermark from '@/pages/Watermark'
-
 export default new VueRouter({
   routes: [
     {
       path: '/',
-      component: Home,
+      component: () => import('@/pages/Home'),
       children: [
         {
           path: 'tree',
           name: 'Tree',
-          component: Tree
+          component: () => import('@/pages/Tree')
         },
         {
           path: 'search-highlight',
           name: 'SearchHighlight',
-          component: SearchHighlight
+          component: () => import('@/pages/SearchHighlight')
         },
         {
           path: 'watermark',
           name: 'Watermark',
-          component: Watermark
+          component: () => import('@/pages/Watermark')
         }
       ]
     }

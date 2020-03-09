d9cb5b76f476bd5104b87ad8b1588734e7ae0715
Lushenggang <lushg-tcxg@qq.com>
2020-03-09 09:21:58 +0800

    push

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
-// const CompressionPlugin = require('compression-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? './' : '/',
   productionSourceMap: false,
-  // configureWebpack: {
-  //   plugins: [
-  //     new CompressionPlugin({
-  //       test: /\.(js|css)?$/i,
-  //       filename: '[path].gz[query]',
-  //       algorithm: 'gzip',
-  //       minRatio: 0.8,
-  //       deleteOriginalAssets: true
-  //     })
-  //   ]
-  // }
+  configureWebpack: {
+    plugins: [
+      new CompressionPlugin({
+        test: /\.(js|css)?$/i,
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        minRatio: 0.8,
+        deleteOriginalAssets: true
+      })
+    ]
+  }
 }

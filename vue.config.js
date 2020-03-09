aa1566d171196256cb54b9fa53e6bd66e942e2a4
Lushenggang <lushg-tcxg@qq.com>
2020-03-09 09:39:33 +0800

    push

--- vue.config.js
+++ vue.config.js
@@ -6,11 +6,11 @@ module.exports = {
   configureWebpack: {
     plugins: [
       new CompressionPlugin({
-        test: /\.(js|css)?$/i,
-        filename: '[path].gz[query]',
-        algorithm: 'gzip',
-        minRatio: 0.8,
-        deleteOriginalAssets: true
+        test: /\.(js|css)?$/i, // 哪些文件要压缩
+        filename: '[path].gz[query]',　// 压缩后的文件名
+        algorithm: 'gzip',　// 使用gzip压缩
+        minRatio: 1,　// 压缩率小于1才会压缩
+        deleteOriginalAssets: true // 删除未压缩的文件，如果希望提供非gzip的资源，可不设置或者设置为false
       })
     ]
   }

b43c6ec15710f24f9cd9413157346ce585596537
lushg <lushg-tcxg@qq.com>
2020-11-23 10:36:27 +0800

    search-highlight

--- src/pages/SearchHighlight.vue
+++ src/pages/SearchHighlight.vue
@@ -16,11 +16,12 @@
           <Button type="primary" size="small" class="button" @click.stop="searchNext">下一个</Button>
         </div>
       </div>
+      <div class="notice">支持对HTML字符串内容搜索，下面的示例是一个html字符串，可以跨标签搜索关键字</div>
       <search-highlight
         class="content-container"
         ref="search"
         @current-change="currentChange"
-        @mactch-count-change="matchCountChange"
+        @match-count-change="matchCountChange"
         :keyword="keyword"
         :content="content">
       </search-highlight>
@@ -39,28 +40,28 @@ export default {
     return {
       currentIdx: 0,
       matchCount: 0,
-      keyword: '月',
+      keyword: '江畔何人初见月',
       content: `
-            春江花月夜
+            春江花<b>月</b>夜
                 [唐] 张若虚
-      春江潮水连海平，海上明月共潮生。
-      滟滟随波千万里，何处春江无月明！
-      江流宛转绕芳甸，月照花林皆似霰；
+      春江潮水连海平，海上明<b>月</b>共潮生。
+      滟滟随波千万里，何处春江无<b>月</b>明！
+      江流宛转绕芳甸，<b>月</b>照花林皆似霰；
       空里流霜不觉飞，汀上白沙看不见。
-      江天一色无纤尘，皎皎空中孤月轮。
-      江畔何人初见月？江月何年初照人？
-      人生代代无穷已，江月年年望相似。
-      不知江月待何人，但见长江送流水。
+      江天一色无纤尘，皎皎空中孤<b>月</b>轮。
+      <span style="color: #4791ff">江畔何人初见<b>月</b>？江<b>月</b>何年初照人？</span>
+      人生代代无穷已，江<b>月</b>年年望相似。
+      不知江<b>月</b>待何人，但见长江送流水。
       白云一片去悠悠，青枫浦上不胜愁。
-      谁家今夜扁舟子？何处相思明月楼？
-      可怜楼上月徘徊，应照离人妆镜台。
+      谁家今夜扁舟子？何处相思明<b>月</b>楼？
+      可怜楼上<b>月</b>徘徊，应照离人妆镜台。
       玉户帘中卷不去，捣衣砧上拂还来。
-      此时相望不相闻，愿逐月华流照君。
+      此时相望不相闻，愿逐<b>月</b>华流照君。
       鸿雁长飞光不度，鱼龙潜跃水成文。
       昨夜闲潭梦落花，可怜春半不还家。
-      江水流春去欲尽，江潭落月复西斜。
-      斜月沉沉藏海雾，碣石潇湘无限路。
-      不知乘月几人归，落月摇情满江树。`,
+      江水流春去欲尽，江潭落<b>月</b>复西斜。
+      斜<b>月</b>沉沉藏海雾，碣石潇湘无限路。
+      不知乘<b>月</b>几人归，落<b>月</b>摇情满江树。`,
     }
   },
 
